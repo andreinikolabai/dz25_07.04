c62a4918e7183ef148ba68ae5e50fdd78a3d1fbe
andreinikolabai <andreiplus2000@gmail.com>
2023-04-07 12:22:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const emojiList = document.getElementById('emoji-list');
+const counterList = document.getElementById('counter-list');
+const emojis = ['😡', '😠', '😐', '😊', '😍'];
+const counters = [0, 0, 0, 0, 0];
+
+let emojiHTML = '';
+let counterHTML = '';
+for (let i = 0; i < emojis.length; i++) {
+    emojiHTML += `<span class="emoji" data-index="${i}">${emojis[i]}</span>`;
+    counterHTML += `<span class="counter">0</span>`;
+}
+emojiList.innerHTML = emojiHTML;
+counterList.innerHTML = counterHTML;
+
+emojiList.addEventListener('click', function(event) {
+    const emojiElement = event.target.closest('.emoji');
+    if (emojiElement) {
+        const emojiIndex = emojiElement.dataset.index;
+        counters[emojiIndex]++;
+        counterList.children[emojiIndex].textContent = counters[emojiIndex];
+    }
+});
